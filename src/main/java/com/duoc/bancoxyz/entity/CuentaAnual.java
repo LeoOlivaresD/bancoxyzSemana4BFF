93dc3819a786e5cc0d3e60f85e46ed09a214f05f
LeoOlivaresD <cspraword@gmail.com>
2025-09-08 22:18:11 -0300

    Se completa clase de cuenta anual

--- src/main/java/com/duoc/bancoxyz/entity/CuentaAnual.java
+++ src/main/java/com/duoc/bancoxyz/entity/CuentaAnual.java
@@ -1,5 +1,37 @@
 package com.duoc.bancoxyz.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cuentas_anuales")
 public class CuentaAnual {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_cuenta")
+    private String idCuenta;
+
+    @Column(name = "fecha_transaccion")
+    private String fecha;
+
+    @Column(name = "tipo_transanccion")
+    private String tipoTransaccion;
+
+    private Integer monto;
+
+    @Column(name = "descripcion_transaccion")
+    private String descTransaccion;
 
 }
