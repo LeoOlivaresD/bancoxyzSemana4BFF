6443113b0b85f54595bfccf5558f88eef9a28ea4
LeoOlivaresD <cspraword@gmail.com>
2025-09-08 22:01:01 -0300

    Commit Inicial

---
+++ src/main/java/com/duoc/bancoxyz/BancoxyzApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.bancoxyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoxyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoxyzApplication.class, args);
+	}
+
+}
