7c0a1213ba8993b5d29c0ecb33400d5182075872
LeoOlivaresD <cspraword@gmail.com>
2025-09-08 22:26:31 -0300

    Se completa clase transaccion

--- src/main/java/com/duoc/bancoxyz/entity/Transaccion.java
+++ src/main/java/com/duoc/bancoxyz/entity/Transaccion.java
@@ -1,5 +1,24 @@
 package com.duoc.bancoxyz.entity;
 
-public class Transaccion {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transacciones")
+public class Transaccion {
+    @Id
+    private Long id;
+    @Column(name = "fecha_transaccion")
+    private String fechaTransaccion;
+    private Integer monto;
+    private String tipo;
 }
